7d8c8c138ce0edaeb2f505beb0aaf5ddc5d636ea
Nervado <evandroabreudeoliveira@gmail.com>
2019-08-22 06:49:42 -0300

    backend gobarber

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'ea144be97e7adfcfb219fbf4069d9984',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
-import UserControllers from './app/controllers/UserControllers';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
-routes.post('/users', UserControllers.store);
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store); // login
+
+routes.put('/users', UserController.update);
 
 export default routes;
