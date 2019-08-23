9676a767d9330e98e2314de31a7b512811afd13c
Nervado <evandroabreudeoliveira@gmail.com>
2019-08-23 06:21:45 -0300

    add schema validation

--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,13 @@ import auth from './app/middleware/auth';
 
 const routes = new Router();
 
+// Create user
 routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store); // login
-
+// Login user
+routes.post('/sessions', SessionController.store);
+// Auth user
 routes.use(auth);
+// Modify user
 routes.put('/users', UserController.update);
 
 export default routes;
