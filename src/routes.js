97bc6bcce7714c1e4e3e2e9fcce4ef29c1faab91
Nervado <evandroabreudeoliveira@gmail.com>
2019-08-25 16:57:24 -0300

    add tables: files, appointments

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 import auth from './app/middleware/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // Create user
 routes.post('/users', UserController.store);
@@ -14,5 +19,9 @@ routes.post('/sessions', SessionController.store);
 routes.use(auth);
 // Modify user
 routes.put('/users', UserController.update);
+// List all Providers
+routes.get('/providers', ProviderController.index);
+// Store file
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
